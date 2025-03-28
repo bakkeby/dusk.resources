c526e9c5f6380848736f682a6d61c2eca810b951
Bakkeby <bakkeby@gmail.com>
2025-03-28 21:21:06 +0100

    playerctl_status.sh - clear status on exit / cleanup

--- scripts/statusbar/playerctl_status.sh
+++ scripts/statusbar/playerctl_status.sh
@@ -3,8 +3,9 @@
 DIR=$(dirname "${BASH_SOURCE[0]}")
 
 cleanup() {
-    pkill -P $$  # Kill all child processes of this script
-    exit 0
+	pkill -P $$  # Kill all child processes of this script
+	duskc --ignore-reply setstatus 8 ""
+	exit 0
 }
 
 "$DIR"/playerctl_updater.sh &
@@ -12,5 +13,5 @@ cleanup() {
 trap 'cleanup' SIGINT SIGTERM EXIT  # Run cleanup when the script is killed
 
 playerctl -F status | while read -r; do
-    kill -USR1 $(pgrep -f playerctl_updater.sh)
+	kill -USR1 $(pgrep -f playerctl_updater.sh)
 done;
